317b3e792fa64c2e2d173e692f44a3cf90122dfb
devesh1808 <devesh1808@gmail.com>
2025-01-28 13:10:50 +0530

    created the login and signup page in Login component, added styling and handled the toggling functionality using useState

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -1,7 +1,92 @@
+import { useState } from "react";
+
+import { Box, TextField, Button, styled, Typography } from "@mui/material";
+
+const Component = styled(Box)`
+  width: 400px;
+  margin: auto;
+  box-shadow: 5px 2px 5px 2px rgb(0 0 0/ 0.6);
+`;
+
+const Image = styled("img")({
+  width: 100,
+  display: "flex",
+  margin: "auto",
+  padding: "50px 0 0",
+});
+
+const Wrapper = styled(Box)`
+  padding: 25px 35px;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  & > div,
+  & > button,
+  & > p {
+    margin-top: 20px;
+  }
+`;
+
+const LoginButton = styled(Button)`
+  text-transform: none;
+  background: #fb641b;
+  color: #fff;
+  height: 48px;
+  border-radius: 2px;
+`;
+
+const SignupButton = styled(Button)`
+  text-transform: none;
+  background: #fff;
+  color: #2874f0;
+  height: 48px;
+  border-radius: 2px;
+  box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
+`;
+
+const Text = styled(Typography)`
+  color: #878787;
+  font-size: 16px;
+`;
+
 const Login = () => {
-    return(
-        <p>Hello from Login</p>
-    )
-}
+  const imageURL =
+    "https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png";
+
+  const [account, toggleAccount] = useState("login");
+
+  return (
+    <Component>
+      <Box>
+        <Image src={imageURL} alt="login" />
+        {account === "login" ? (
+          <Wrapper>
+            <TextField variant="standard" label="Enter username" />
+            <TextField variant="standard" label="Enter password" />
+            <LoginButton variant="contained">Login</LoginButton>
+            <Text>OR</Text>
+            <SignupButton onClick={() => toggleAccount("signup")}>
+              Create an account
+            </SignupButton>
+          </Wrapper>
+        ) : (
+          <Wrapper>
+            <TextField variant="standard" label="Enter Name" />
+            <TextField variant="standard" label="Enter Username" />
+            <TextField variant="standard" label="Enter Password" />
+            <SignupButton>Signup</SignupButton>
+            <Text>OR</Text>
+            <LoginButton
+              variant="contained"
+              onClick={() => toggleAccount("login")}
+            >
+              Already have an account
+            </LoginButton>
+          </Wrapper>
+        )}
+      </Box>
+    </Component>
+  );
+};
 
 export default Login;
