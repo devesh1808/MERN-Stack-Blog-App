a9a7f94f230701f7872cbd759c0b251b69b2c860
devesh1808 <devesh1808@gmail.com>
2025-02-08 23:37:30 +0530

    created the .env file to store the username and password and use these variables inside the db url, instead of using direct value

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
 
 import Connection from "./database/db.js";
+
+dotenv.config();
 
 const app = express();
 
@@ -10,4 +13,7 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-Connection();
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+Connection(USERNAME, PASSWORD);
